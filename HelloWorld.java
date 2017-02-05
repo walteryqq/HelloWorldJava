a8fb0d208bf36f202d7428edee64b21249c2876f
walter <congyi.zhu@foxmail.com>
2017-02-05 21:53:08 +0800

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package com.Walter.Hello;
+
+/**
+ * Created by walter on 2/5/17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld！");
+        System.out.println("HelloWorldAgain!");
+    }
+}
